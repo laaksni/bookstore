0abd9b1aa87454cece7ae9900a1a915f683e1d91
laaksni <niko.laaksonen@hotmail.com>
2024-09-17 04:13:31 +0300

    booklist updated

--- src/main/java/hh/s24/bookstore/web/BookController.java
+++ src/main/java/hh/s24/bookstore/web/BookController.java
@@ -1,16 +1,21 @@
 package hh.s24.bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import hh.s24.bookstore.domain.BookRepository;
+
 @Controller
 public class BookController {
+    @Autowired
+    private BookRepository repository;
 
-    @RequestMapping("/bookstore")
+    @RequestMapping("/booklist")
     public String bookList(Model model) {
-        model.addAttribute("message", "Welcome to the Bookstore!");
-        return "bookstore";
+        model.addAttribute("books", repository.findAll());
+        return "booklist";
     }
     
 
