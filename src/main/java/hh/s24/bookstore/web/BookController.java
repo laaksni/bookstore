85f178d6be059afbe25275bf0d0981be6984b48a
laaksni <niko.laaksonen@hotmail.com>
2024-09-17 05:11:16 +0300

    add/delete updated

--- src/main/java/hh/s24/bookstore/web/BookController.java
+++ src/main/java/hh/s24/bookstore/web/BookController.java
@@ -3,9 +3,15 @@ package hh.s24.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import hh.s24.bookstore.domain.Book;
 import hh.s24.bookstore.domain.BookRepository;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+
 
 @Controller
 public class BookController {
@@ -17,6 +23,25 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
+
+    @RequestMapping(value = "/add")
+    public String addBook(Model model) {
+        model.addAttribute("book", new Book());
+        return "addbook";
+    }
+    
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book) {
+        repository.save(book);
+        return "redirect:/booklist";
+    }
+
+    @RequestMapping(value = "/delete/{id}", method=RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+        repository.deleteById(bookId);
+        return "redirect:/booklist";
+    }
+    
     
 
 }
